08b52e4c627a28d1dc7fe13345a5dcf580e981c2
Shailendra <shailendraordeepak@gmail.com>
2024-02-27 10:09:55 +0530

    hry

---
+++ src/page/index.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='root'>
+        <div className='image'>
+          <span className='cri-pumpswins'>
+            C.R.I.PUMPSWINS THE NATIONALENERGY CONSERVATIONAWARD 2O18fOr
+            the4thtime.
+          </span>
+          <div className='image-1' />
+          <div className='image-2' />
+          <div className='image-3' />
+          <span className='cri-fluid-systems'>C.R.I. FLUID SYSTEMS</span>
+          <span className='pumping-trust-worldwide'>
+            Pumping trust. Worldwide.
+          </span>
+        </div>
+        <span className='cri-energy-efficient'>
+          C.R.i.'s energyefficientproductsarewellrecognizedby various
+          GovernmentInstitutions,astrustworthy productsfor
+          <br />
+          various projects across theglobe to save energy
+          <br />
+          ·C.R.I. is thehighest contributorin the country for the projects of
+          EEsL(EnergyEfficiency Services Limited)to replace the
+          <br />
+          old inefficient pumps with 5 Star rated energy efficient smart pumps
+          withIoT enabled control panel
+        </span>
+        <div className='groups'>
+          <div className='image-4' />
+        </div>
+        <div className='image-5' />
+        <div className='groups-6'>
+          <span className='government-of-in'>
+            Government of India has awarded the"National Energy Conservation
+            Award 2o18".Mr. G. Selvaraj, Joint ManagingDirector
+            <br />
+            of C.R.I. Group received the award from Smt. Sumitra Mahajan,
+            Speaker of Lok Sabha & Shri. RajKumar Singh,Honorable
+            <br />
+            Minister ofState.
+          </span>
+          <span className='ed-over-1-lakhs-star-rated-pumpsets-across-the-country'>
+            EDOVER1OLAKHSSTARRATEDPUMPSETSACROSSTHECOUNTRYRESULTINGINACUMULATIVESAVINGOFMORETHAN9,OOOMILLIONUNITSOFPOWERFORTHENATION
+          </span>
+          <div className='flex-row-fb'>
+            <div className='image-7' />
+            <div className='image-8' />
+          </div>
+          <span className='valves-pumps-pi'>
+            Valves -Pumps-Pipes-IoT Drives & Controllers -Wires & Cables-Solar
+            Systems-Motors
+          </span>
+          <span className='cri-fluid-systems-9'>
+            C.R.I.FLUIDSYSTEMSPRODUCTSCATERTODIVERSESEGMENTS
+          </span>
+          <div className='flex-row-dcf'>
+            <div className='image-a' />
+            <span className='chemicals-process'>
+              CHEMICALSSPROCESSPOWERWATER&WASTEWATEROILS&GASPHARMASUGARSSDISTILLERIESPAPER&PULPMARINE&DEFENCEMETAL&MININGFOOD8BEVERAGE
+              <br />
+              PETROCHEMICALSREFINERIESSOLARBUILDINGHVACFIREFIGHTINGAGRICULTURESRESIDENTIAL
+            </span>
+          </div>
+        </div>
+        <div className='image-b'>
+          <div className='image-c' />
+          <div className='image-d' />
+          <div className='image-e' />
+          <span className='toll-free'>Tollfree18002001234</span>
+          <span className='facebook-link'>www.facebook.com/cripumps</span>
+          <span className='cri-groups'>www.crigroups.com</span>
+        </div>
+      </div>
+      <div className='image-f' />
+    </div>
+  );
+}
